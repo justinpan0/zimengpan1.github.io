42d15e31482ef8d1114f0bb389ea8da58fcea344
zimengpan <zimengpan@gmail.com>
2017-01-28 13:01:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 function main(){
  	$('.skillset').hide();
   $('.skillset').fadeIn(1000);
+  $('.projects').hide();
+  $('.projects-button').on('click', function(){
+    $('.projects').toggle();
+  });
 }
 
 $(document).ready(main);
