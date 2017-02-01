e4032e29d4c57bd623320bc6e9e42f1dbf092766
zimengpan <zimengpan@gmail.com>
2017-02-01 15:11:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,51 @@
 function main(){
- 	$('.skillset').hide();
-  $('.skillset').fadeIn(1000);
-  $('.projects').hide();
-  $('.projects-button').on('click', function(){
+ 	//$('.skillset').hide();
+  //$('.skillset').fadeIn(1000);
+  //$('.projects').hide();
+  //$('.projects-button').on('click', function(){
     //$(this).next().toggle();
-    $(this).next().slideToggle(400);
-    $(this).toggleClass('active');
-    $(this).text('Projects Viewed');
+    //$(this).next().slideToggle(400);
+    //$(this).toggleClass('active');
+    //$(this).text('Projects Viewed');
+  var colorClass = '';
+  
+  $('.select-color').on('click', function(){
+    var selectedColor = $(this).attr('class');
+    
+    switch(selectedColor){
+      case 'select-color cyan not-selected':
+        colorClass = 'cyan';
+        break;
+      case 'select-color yellow not-selected':
+        colorClass = 'yellow';
+        break;
+      case 'select-color magenta not-selected':
+        colorClass = 'magenta';
+        break;
+    }
+    
+    $(this).removeClass('not-selected');
+    //make sure that all the other elements next to the clicked element receive the not-selected class.
+    $(this).siblings().addClass('not-selected');
+  });
+  
+  $('.box').on('click', function(){
+  	$(this).toggleClass(colorClass);        
+  });
+  
+  $('.toggle-blink').on('click', function(){
+    if(colorClass){
+      $('.toggle-blink').toggleClass('opacity');
+      var inter1 = setInterval(function() {
+      	$('.box.magenta, .box.cyan, .box.yellow').toggleClass('blink');
+  		}, 350);
+      $('.toggle-blink').on('click', function(){
+        clearInterval(inter1);
+      });
+    }
   });
 }
+
+
 
 $(document).ready(main);
